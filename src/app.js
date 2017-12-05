c932d8491abc5f074ab3807ae04605e3bdbb53c6
ShangYunliang <1071332303@qq.com>
2017-12-05 18:51:34 +0800

    select dom

--- src/app.js
+++ src/app.js
@@ -1,31 +1,11 @@
-d3.json('data/data.json', (data) =>{
-  const max = d3.max(data, (d) => {
-    return d.age
-  })
-  console.log(`max : ${max}`);
-  
-  const min = d3.min(data, (d) => {
-    return d.age
-  })
-  console.log(`min : ${min}`);
-  
-  const extent = d3.extent(data, (d) => d.age)
-  console.log(`extent : ${extent}`);
-  
-  const scale = d3.scaleLinear()
-    .domain(extent)
-    .range([0, 600])
-    .clamp(true)
-  console.log(scale(37));
-  
-  var ages = d3.set(data, (d) => d.age) 
-  console.log(ages.values());
-})
+var div = d3.select('div');
+console.log(div.nodes());
 
-d3.csv('data/data.csv', (data) =>{
-  console.log(data);
-})
+var divLinks = div.selectAll('a');
+console.log(divLinks.nodes());
 
-d3.csv('data/data.tsv', (data) =>{
-  console.log(data);
-})
+var secondLink = d3.selectAll('a:nth-child(2)');
+console.log(secondLink.nodes());
+
+var allLinks = d3.selectAll(document.links);
+console.log(allLinks.size());
