1be1a9413b106a52d1de992432871421d5a197cc
ShangYunliang <1071332303@qq.com>
2017-12-05 16:45:21 +0800

    02-timeScale

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+function drawLine(lineString) {
+  console.log(`------------------------${lineString}--------------------------`);
+}
+
+drawLine('scaleLinear')
 var linearScale = d3.scaleLinear()
   .domain([0, 100])
   .range([0, 600])
@@ -9,3 +14,18 @@ console.log(linearScale(50))
 console.log(linearScale(100))
 
 console.log(linearScale.invert(300));
+
+drawLine('scaleTime')
+// 注意时间月份
+var timeScale = d3.scaleTime()
+  .domain([new Date(2016,0,1), new Date(2016,9,31)])
+  .range([0, 100])
+  .clamp(true)
+  
+console.log(timeScale(new Date()))
+console.log(timeScale(new Date(2016,0,1)))
+
+
+console.log(timeScale.invert(50))
+console.log(timeScale.invert(0))
+console.log(timeScale.invert(100));
