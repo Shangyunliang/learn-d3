bd44617be17447b9a0e8038ba58fc7c77faa8490
ShangYunliang <1071332303@qq.com>
2017-12-06 14:34:58 +0800

    08 create dom

--- src/app.js
+++ src/app.js
@@ -11,9 +11,34 @@ var allLinks = d3.selectAll(document.links);
 console.log(allLinks.size());
 
 
-var secondLink = d3.select('a:nth-child(2)')
-secondLink.attr('href', 'https://www.baidu.com')
-  // .style('color', 'red')
-  .classed('red', true)
-  .text('inventory')
-  .html('inventory <b>SALE</b>')
+// var secondLink = d3.select('a:nth-child(2)')
+// secondLink.attr('href', 'https://www.baidu.com')
+//   // .style('color', 'red')
+//   .classed('red', true)
+//   .text('inventory')
+//   .html('inventory <b>SALE</b>')
+  
+// d3.select('.title')
+//   .append('button')
+//     .html('inventory <b>SALE</b>')
+    
+    
+// d3.select('.title')
+//   .insert('div', 'a:nth-child(1)')
+//     .html('inventory <b>SALE</b>')
+// 
+// d3.select('.action').remove()
+
+
+d3.select('.title')
+  .append('div')
+    .style('color', 'red')
+    .html('Inventory <b>SALE </b>')
+  // .append('button')   这样直接调用追加的button会在上面创建的div中
+  //   .style('display', 'block')
+  //   .text('submit')
+    
+d3.select('.title')
+  .append('button')
+    .style('display', 'block')
+    .text('submit')
