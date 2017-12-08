8091df98ccbfd97382c2cb1a955ac193f631e470
ShangYunliang <1071332303@qq.com>
2017-12-08 13:41:38 +0800

    finish lesson 16

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@
 // g中内容的就可以正常使用上面计算的出来的width 和 height了
 
 
-const margin = {top: 20, right: 15, bottom: 20, left: 15}
-let width = 425 - margin.left - margin.right
+const margin = {top: 20, right: 20, bottom: 40, left: 40}
+let width = 525 - margin.left - margin.right
 let height = 625 - margin.top - margin.bottom
 
 const svg = d3.select('.chart')
@@ -19,14 +19,33 @@ const svg = d3.select('.chart')
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 svg.append('rect')
-  .attr('width', width / 2 )
+  .attr('width', width)
   .attr('height', height)
   .style('fill', 'lightblue')
   .style('stroke', 'green')
 
-svg.append('rect')
-  .attr('x', width / 2)
-  .attr('width', width / 2)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green')
+// 这里svg 仍然指向g 如果写成 var a = svg.append('rect') a 指向rect
+
+const yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0])
+
+const yAxis = d3.axisLeft(yScale)
+  // .ticks(5) // 5个坐标
+  // .tickValues([10,20,40,88,100]) // 这些值必须展示
+  // .ticks(5, '%') 百分比 domain最大为1
+svg.call(yAxis)
+
+const xScale = d3.scaleTime()
+  .domain([new Date(2017, 0, 1, 0), new Date(2017, 0, 2, 0)])
+  .range([0, width])
+
+const xAxis = d3.axisBottom(xScale)
+  .ticks(6) // 仅仅是一种建议， d3 会根据最好的方案来替代这个。
+  .tickSize(10)
+  .tickPadding(5)
+
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis)
