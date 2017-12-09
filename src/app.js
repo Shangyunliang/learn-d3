145589012f8de48b7ac53dc28e3faf20f6200c61
ShangYunliang <1071332303@qq.com>
2017-12-09 14:41:00 +0800

    完成了circle的绘画

--- src/app.js
+++ src/app.js
@@ -23,67 +23,49 @@ const svg = d3.select('.chart')
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-var data = [
-  {score: 63, subject: 'Mathematics'},
-  {score: 82, subject: 'Geography'},
-  {score: 74, subject: 'Spelling'},
-  {score: 97, subject: 'Reading'},
-  {score: 52, subject: 'Science'},
-  {score: 74, subject: 'Chemistry'},
-  {score: 100, subject: 'Physics'},
-  {score: 52, subject: 'ASL'}
-];
+d3.json('/data.json', function(err, data){
+  // 创建一个线性比例尺
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.expectancy))
+    .range([height, 0])
+    .nice()
 
-// 这里svg 仍然指向g 如果写成 var a = svg.append('rect') a 指向rect
+  // 将这个比例尺转换成纵坐标
+  const yAxis = d3.axisLeft(yScale)
+    // .ticks(5) // 5个坐标
+    // .tickValues([10,20,40,88,100]) // 这些值必须展示
+    // .ticks(5, '%') 百分比 domain最大为1
+  svg.call(yAxis)
 
-// 创建一个线性比例尺
-const yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height, 0])
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.cost))
+    .range([0, width])
+    .nice()
 
-// 将这个比例尺转换成纵坐标
-const yAxis = d3.axisLeft(yScale)
-  // .ticks(5) // 5个坐标
-  // .tickValues([10,20,40,88,100]) // 这些值必须展示
-  // .ticks(5, '%') 百分比 domain最大为1
-svg.call(yAxis)
+  const xAxis = d3.axisBottom(xScale)
+    .ticks(5)
 
-const xScale = d3.scaleBand()
-  .padding(0.2)
-  .domain(data.map(d => d.subject))
-  .range([0, width])
+  svg
+    .append('g')      // 这里之所以要从新添加一个g， 是因为axisBottom只是创建一个锯齿朝下的底部坐标样子， 并不能代表在底部。
+      .attr('transform', `translate(0, ${height})`)
+    .call(xAxis)
 
-const xAxis = d3.axisBottom(xScale)
-  .ticks(6) // 仅仅是一种建议， d3 会根据最好的方案来替代这个。
-  .tickSize(10)
-  .tickPadding(5)
+  var rScale = d3.scaleSqrt()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([0, 40])
 
-svg
-  .append('g')      // 这里之所以要从新添加一个g， 是因为axisBottom只是创建一个锯齿朝下的底部坐标样子， 并不能代表在底部。
-    .attr('transform', `translate(0, ${height})`)
-  .call(xAxis)
-  .selectAll('text')
-  .style('text-anchor', 'end')
-  .attr('transform', 'rotate(-45)')
+  svg
+    .selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.cost))
+    .attr('cy', d => yScale(d.expectancy))
+    .attr('r', d => rScale(d.population))
+    .style('fill-opacity', 0.5)
+    .style('fill', 'steelblue')
 
-svg.selectAll('rect')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('x', d => {
-    console.log(xScale(d.subject));
-    return xScale(d.subject)
-  })
-  .attr('y', d => yScale(d.score))
-  .attr('width', d => {
-    console.log('xScale.bandwidth()', xScale.bandwidth());
-    return xScale.bandwidth()
-  })
-  .attr('height', d => {
-    console.log(yScale(d.score));
-    // yScale(d.score) 拿到的是从上到下的高度。 从100 到分数的高度
-    return height - yScale(d.score)
-  })
+})
 
 function responsivefy(svg){
   // get container + svg aspepct ratio
